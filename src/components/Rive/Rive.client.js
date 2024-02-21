9916ad6fc5745182c36e1caab6bc27f42b616073
Luuk <luukbrauckmann@gmail.com>
2024-02-21 15:42:44 +0100

    Merge main

--- src/components/Rive/Rive.client.js
+++ src/components/Rive/Rive.client.js
@@ -1,28 +1,14 @@
-import { Alignment, Fit, Layout, Rive, RuntimeLoader } from "@rive-app/canvas";
-import riveWASMResource from "@rive-app/canvas/rive.wasm?url";
-
-RuntimeLoader.setWasmUrl(riveWASMResource);
+import RiveWorker from "./Rive.worker.js?worker";
 
 class RiveCanvas extends HTMLElement {
   constructor() {
     super();
 
     const canvas = this.querySelector("canvas");
+    const offscreen = canvas.transferControlToOffscreen();
 
-    const { src, fit, alignment, autoplay, statemachines } = canvas.dataset;
-
-    const layout = new Layout({
-      fit: Fit[fit],
-      alignment: Alignment[alignment],
-    });
-
-    const rive = new Rive({
-      canvas,
-      src,
-      layout,
-      autoplay: typeof autoplay != "undefined",
-      stateMachines: statemachines,
-    });
+    const worker = new RiveWorker();
+    worker.postMessage({ canvas: offscreen, ...canvas.dataset }, [offscreen]);
   }
 }
 
